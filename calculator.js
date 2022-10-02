6f0df842ba2575d0f3ce80970c1443ab85b38f82
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2022-10-02 10:37:26 -0400

    all operations working (using )

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
-(function foo() {
-  console.log('welcome');
-})();
+document.addEventListener('DOMContentLoaded', function (e) {
+  document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let first = document.querySelector('#first_operand').value;
+    let second = document.querySelector('#second_operand').value;
+    let operation = document.querySelector('#operation').value;
+    let output = document.querySelector('#output');
+
+    let result = eval(first + operation + second);
+
+    output.textContent = result;
+  });
+});
